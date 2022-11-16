ded1d9f460b0ee693415b8d180da266adfa0b36c
Sabilillah <sabilillah.dev@gmail.com>
2022-11-16 19:58:40 +0700

    Create isPreload state

--- src/states/index.js
+++ src/states/index.js
@@ -1,6 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authUserReducer from "./authUser/reducer";
+import isPreLoadReducer from "./isPreload/reducer";
 
-const store = configureStore({ reducer: { authUser: authUserReducer } });
+const store = configureStore({
+  reducer: {
+    authUser: authUserReducer,
+    isPreload: isPreLoadReducer,
+  },
+});
 
 export default store;
