c47675ad609b1b7ace3c5c52ca3b7c550fd68f6d
Sabilillah <sabilillah.dev@gmail.com>
2022-11-18 17:24:34 +0700

    Create category list component

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import CategoryButton from "./CategoryButton";
+
+function CategoryList() {
+  return (
+    <div
+      className="absolute z-10 h-[95vw] min-w-[20px] max-w-[40px] origin-top-left translate-x-2 -rotate-90
+          select-none overflow-y-auto"
+    >
+      <div className="flex translate-y-3 rotate-90 flex-row flex-nowrap gap-x-4 text-white">
+        <CategoryButton />
+        <CategoryButton />
+        <CategoryButton />
+        <CategoryButton />
+        <CategoryButton />
+        <CategoryButton />
+        <CategoryButton />
+        <CategoryButton />
+        <CategoryButton />
+      </div>
+    </div>
+  );
+}
+
+export default CategoryList;
