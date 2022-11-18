7cd6ce26485aa3ec0069855eab9423f97a58a70a
Sabilillah <sabilillah.dev@gmail.com>
2022-11-18 17:22:29 +0700

    Create category button component

---
+++ src/components/CategoryButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CategoryButton() {
+  return (
+    <button className="border border-captain-blue rounded p-1 text-xs whitespace-nowrap">
+      #lorem-ipsum
+    </button>
+  );
+}
+
+export default CategoryButton;
