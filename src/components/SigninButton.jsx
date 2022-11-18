9075c1c0d1046de9bd27d8de74b53c5d9c6fdb48
Sabilillah <sabilillah.dev@gmail.com>
2022-11-18 17:15:55 +0700

    Create signin & signup button component

---
+++ src/components/SigninButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SigninButton() {
+  return <button className="px-2 py-1 text-sm">Sign in</button>;
+}
+
+export default SigninButton;
---
+++ src/components/SignupButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignupButton() {
+  return (
+    <button className="border border-white/50 hover:text-white/80 rounded-md px-2 py-1 text-sm font-bold">
+      Sign up
+    </button>
+  );
+}
+
+export default SignupButton;
